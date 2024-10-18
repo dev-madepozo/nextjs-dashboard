e0ecf1f42630d4d185f9d6c4f1a13e5319165e91
Manuel Del Pozo <madepozo.dev@gmail.com>
2024-10-18 01:38:47 -0500

    Complete chapter 12

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,8 +1,8 @@
 'use server'
 
-import { sql } from '@vercel/postgres'
-import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
+import { revalidatePath } from 'next/cache'
+import { sql } from '@vercel/postgres'
 import { z } from 'zod'
 
 const FormSchema = z.object({
@@ -38,4 +38,30 @@ export async function createInvoice(formData: FormData) {
 
   revalidatePath('/dashboard/invoices')
   redirect('/dashboard/invoices')
+}
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true })
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status')
+  })
+
+  const amountInCents = amount * 100
+
+  await sql`
+    UPDATE invoices
+    SET customer_id=${customerId}, amount=${amountInCents}, status=${status}
+    WHERE id=${id}
+  `
+
+  revalidatePath('/dashboard/invoices')
+  redirect('/dashboard/invoices')
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`
+  revalidatePath('/dashboard/invoices')
 }
