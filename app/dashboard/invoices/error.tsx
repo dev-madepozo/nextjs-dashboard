0f580bc416d432d031d29a53bb21151dadadea47
Manuel Del Pozo <madepozo.dev@gmail.com>
2024-10-19 23:03:38 -0500

    Complete chapter 14

--- app/dashboard/invoices/error.tsx
+++ app/dashboard/invoices/error.tsx
@@ -7,6 +7,7 @@ export default function Error(
     reset: () => void
   }
 ) {
+  console.log(error)
   return (
     <main className="flex h-full flex-col items-center justify-center">
       <h2 className="text-center">Something went wrong!</h2>
